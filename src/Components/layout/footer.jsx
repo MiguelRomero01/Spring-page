68b55ead253cd193af7b613c269dd0d17f153cbb
miguel angel romero parra <mr9501459@gmail.com>
2024-11-21 20:31:54 -0500

    footer update

--- src/Components/layout/footer.jsx
+++ src/Components/layout/footer.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import styles from "./footer.module.css"; 
 import { Link } from 'react-router-dom';  //Routers to pages
 
+//Icons
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import XIcon from '@mui/icons-material/X';
+import InstagramIcon from '@mui/icons-material/Instagram';
+
 function Footer() {
      return (
           <footer className={styles.footer}> 
@@ -13,11 +19,11 @@ function Footer() {
                          <li><Link to=''>Términos y condiciones</Link></li>
                     </ul>
                     <div className={styles.footerIcons}>
-                         <a href="#linkedin" aria-label="LinkedIn" className={styles.iconLinkedin}></a>
-                         <a href="#twitter" aria-label="X" className={styles.iconTwitter}></a>
-                         <a href="#instagram" aria-label="Instagram" className={styles.iconInstagram}></a>
-                         <a href="#youtube" aria-label="YouTube" className={styles.iconYoutube}></a>
-                         <a href="https://discord.gg/TbSCNph5" aria-label="Discord"className={styles.iconDiscord}><img src="/Images/Home/components/LogoDiscord.png"/></a>
+                         <a href="#linkedin" aria-label="LinkedIn" className={styles.iconLinkedin}><LinkedInIcon/></a>
+                         <a href="#twitter" aria-label="X" className={styles.iconTwitter}><XIcon/></a>
+                         <a href="#instagram" aria-label="Instagram" className={styles.iconInstagram}><InstagramIcon/></a>
+                         <a href="#youtube" aria-label="Youtube" className={styles.iconYoutube}><YouTubeIcon/></a>
+                         <a href="https://discord.gg/TbSCNph5" aria-label="Discord"className={styles.iconDiscord}><img src="/Images/Home/components/LogoDiscord.png" alt="discord"/></a>
                     </div>
                </div>
           </footer>
